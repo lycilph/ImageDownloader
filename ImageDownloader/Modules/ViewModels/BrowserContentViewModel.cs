369de6151dd01e58dc647138ee61b5159fcd373a
Ly Cilph <lycilph@gmail.com>
2014-03-05 21:07:27 +0100

    Added the CefSharp.wfp browser nuget package

---
+++ ImageDownloader/Modules/ViewModels/BrowserContentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ImageDownloader.Core;
+using System.ComponentModel.Composition;
+
+namespace ImageDownloader.Modules.ViewModels
+{
+    [Export(typeof(ILayoutItem))]
+    public class BrowserContentViewModel : Content
+    {
+        public BrowserContentViewModel()
+        {
+            DisplayName = "Browser";
+        }
+    }
+}
